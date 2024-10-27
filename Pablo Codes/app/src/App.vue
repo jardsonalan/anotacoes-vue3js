b22f295b2ba83b2575a601d069f7a0d39aa7c8b5
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-10-27 19:07:41 -0300

    subindo projeto

---
+++ Pablo Codes/app/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
