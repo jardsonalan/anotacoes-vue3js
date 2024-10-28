3dca7dbe270c0376a89d74466c7eec67bcb8210e
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-10-27 21:47:15 -0300

    update: métodos

--- Pablo Codes/app/src/components/OUsuario.vue
+++ Pablo Codes/app/src/components/OUsuario.vue
@@ -1,23 +1,34 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { ref } from 'vue';
 
 // Ref: específico para tipos primitivos: string, number, boolean
-// const nome = ref('Jardson Alan');
-// const dataNascimento = ref(0);
-// const idade = ref(19);
+const nome = ref('Jardson Alan');
+const dataNascimento = ref(0);
+const idade = ref(0);
+const idadeFutura = ref(0);
 
 // Alterar o valor da propriedade (idade)
 // idade.value = 38;
 // console.log(idade.value);
 
 // Reactive: focado em objetos
-const pessoa = reactive ({
-    nome: '',
-    dataNascimento: 0,
-    idade: 0
-});
+// const pessoa = reactive ({
+//     nome: '',
+//     dataNascimento: 0,
+//     idade: 0
+// });
 // Alterar o valor da propriedade (idade)
-console.log(pessoa.idade = 19);
+// console.log(pessoa.idade = 19);
+
+const calcularIdade = ():number => {
+    idade.value = new Date().getFullYear() - dataNascimento.value;
+    return idade.value;
+};
+
+const calcularIdadeParametro = (valor:number):number => {
+    idadeFutura.value = idade.value + valor;
+    return idadeFutura.value;
+};
 
 </script>
 
@@ -25,13 +36,17 @@ console.log(pessoa.idade = 19);
     <div>
         <form class="formulario">
             <label for="nome">Nome:</label><br>
-            <input type="text" id="nome" name="nome" v-model="pessoa.nome" /><br/>
+            <input type="text" id="nome" name="nome" v-model="nome" /><br/>
             <label for="dataNascimento">Data de Nascimento:</label><br>
-            <input type="number" id="dataNascimento" name="dataNascimento" v-model="pessoa.dataNascimento" /><br/>
+            <input type="number" id="dataNascimento" name="dataNascimento" v-model="dataNascimento" /><br/>
         </form>
     </div>
 
-    <p style="text-align: center; margin: 20px 0;">{{ pessoa.nome }} nasceu em {{ pessoa.dataNascimento }}</p>
+    <button class="botao" v-on:click="calcularIdade()">Calcula idade</button>
+    <p style="text-align: center; margin: 20px 0;">{{ nome }} tem em {{ idade }} anos</p>
+
+    <button class="botao" v-on:click="calcularIdadeParametro(2)">+2</button>
+    <p style="text-align: center; margin: 20px 0;">{{ nome }} daqui a 2 anos terá {{ idadeFutura }} anos</p>
 </template>
 
 <style scoped>
@@ -48,5 +63,19 @@ console.log(pessoa.idade = 19);
     #nome, #dataNascimento {
         margin: 10px 0;
         width: 100%;
+    }
+
+    .botao {
+        margin: 5px auto;
+        padding: 5px;
+        display: block;
+        background-color: darkcyan;
+        border-radius: 5px;
+        border-style: none;
+        cursor: pointer;
+    }
+
+    .botao:hover {
+        background-color: rgb(182, 147, 147);
     }
 </style>
