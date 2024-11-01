3f861a1f1d407098322953602329104df9759763
Jardson Alan <138258644+jardsonalan@users.noreply.github.com>
2024-10-31 22:02:35 -0300

    update: template refs; defineExpose

--- Pablo Codes/app/src/components/OAviso.vue
+++ Pablo Codes/app/src/components/OAviso.vue
@@ -3,6 +3,14 @@ import { inject } from 'vue'
 
 // Inject
 const aviso = inject('aviso')
+
+// Template Refs
+const chamaAlert = () => {
+  alert('Clicou');
+};
+
+// Expõe para o componente pai os membros e métodos internos de um componente
+defineExpose({aviso, chamaAlert});
 </script>
 
 <template>
--- Pablo Codes/app/src/view/PaginaNaoEncontrada.vue
+++ Pablo Codes/app/src/view/PaginaNaoEncontrada.vue
@@ -1,8 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted, provide } from 'vue';
+import OAviso from '@/components/OAviso.vue';
+
+// Template Refs
+const titulo = ref(null);
+const avisoRef = ref(null);
+
+onMounted(() => {
+    console.log(titulo.value);
+    titulo.value.style.color = 'blue';
+    // titulo.value.innerText = 'Alterado';
+    console.log(avisoRef.value.aviso);
+});
+
+const mostraMensagem = () => {
+    avisoRef.value.chamaAlert();
+};
+
+provide('aviso', 'Página inválida');
+</script>
 
 <template>
     <div>
-        <h1>Página não encontrada!</h1>
+        <!-- Template Refs -->
+        <h1 ref="titulo">Página não encontrada!</h1>
+        <OAviso ref="avisoRef"/>
+        <button @click="mostraMensagem()">Chama Alert</button>
     </div>
 </template>
 
